602a289e2bd9412dbe7eda017bd810428003c75f
seonggul <gul930429@gmail.com>
2020-12-22 19:22:15 +0900

    modify_api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 const Axios = require("axios");
 
-let totalData, dayData, dataForClient, date;
+let dayData, dataForClient, date;
 
 const getDate = () => {
   let year = new Date().getFullYear();
@@ -17,65 +17,6 @@ const getDate = () => {
   }
   date = String(year) + String(month) + String(day);
   console.log(date);
-};
-
-const get7Day = async () => {
-  const {
-    data: {
-      response: {
-        body: {
-          items: { item },
-        },
-      },
-    },
-  } = await Axios.get(
-    "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson",
-    {
-      params: {
-        ServiceKey:
-          "dsbBi7zlYOjLk7SS4STAgIi3cpFgqr7RzsDUJzR5duwKHYPfuEPmA5Hh6zsxJpzTfhFqdoUCwXT/G0SuYxnpUg==",
-        startCreateDt: date - 7,
-        endCreateDt: date,
-      },
-    }
-  );
-  if (item === undefined || null) {
-    const {
-      data: {
-        response: {
-          body: {
-            items: { item },
-          },
-        },
-      },
-    } = await Axios.get(
-      "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson",
-      {
-        params: {
-          ServiceKey:
-            "dsbBi7zlYOjLk7SS4STAgIi3cpFgqr7RzsDUJzR5duwKHYPfuEPmA5Hh6zsxJpzTfhFqdoUCwXT/G0SuYxnpUg==",
-          startCreateDt: date - 8,
-          endCreateDt: date - 1,
-        },
-      }
-    );
-    try {
-      totalData = item.map((a) => {
-        return { key: a.stateDt, decideCnt: a.decideCnt };
-      });
-    } catch (error) {
-      console.log(`error : ${error}`);
-    }
-  } else {
-    try {
-      totalData = item.map((a) => {
-        return { key: a.stateDt, decideCnt: a.decideCnt };
-      });
-    } catch (error) {
-      console.log(`error : ${error}`);
-    }
-    console.log(totalData);
-  }
 };
 
 const getDay = async () => {
@@ -93,12 +34,11 @@ const getDay = async () => {
       params: {
         ServiceKey:
           "dsbBi7zlYOjLk7SS4STAgIi3cpFgqr7RzsDUJzR5duwKHYPfuEPmA5Hh6zsxJpzTfhFqdoUCwXT/G0SuYxnpUg==",
-        startCreateDt: date,
+        startCreateDt: date - 7,
         endCreateDt: date,
       },
     }
   );
-  console.log(item);
   if (item === undefined || null) {
     date = date - 1;
     const {
@@ -115,8 +55,8 @@ const getDay = async () => {
         params: {
           ServiceKey:
             "dsbBi7zlYOjLk7SS4STAgIi3cpFgqr7RzsDUJzR5duwKHYPfuEPmA5Hh6zsxJpzTfhFqdoUCwXT/G0SuYxnpUg==",
-          startCreateDt: date,
-          endCreateDt: date,
+          startCreateDt: date - 8,
+          endCreateDt: date - 1,
         },
       }
     );
@@ -125,7 +65,7 @@ const getDay = async () => {
         let b = a.createDt.substr(0, 10).split("-");
         let c = b[0] + b[1] + b[2];
         return {
-          key: a.gubunEn,
+          key: c + a.gubunEn,
           createDt: c,
           gubun: a.gubun,
           defCnt: a.defCnt,
@@ -144,7 +84,7 @@ const getDay = async () => {
         let c = b[0] + b[1] + b[2];
 
         return {
-          key: a.gubunEn,
+          key: c + a.gubunEn,
           createDt: c,
           gubun: a.gubun,
           defCnt: a.defCnt,
@@ -157,7 +97,6 @@ const getDay = async () => {
       console.log(`error : ${error}`);
     }
   }
-  console.log(dayData);
 };
 
 (async () => {
@@ -166,9 +105,9 @@ const getDay = async () => {
   app.use(cors());
 
   await getDate();
-  await get7Day();
   await getDay();
-  dataForClient = await { date, totalData, dayData };
+
+  dataForClient = await { date, dayData };
   app.get("/", (req, res) => {
     res.send(dataForClient);
   });
